afb77a31a9d959a577efdafa681ffa0d1fb61420
Ceyhun <admnsts@hotmail.com>
2024-11-20 04:02:07 +0300

    author added to post

--- routes/main.js
+++ routes/main.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const Post = require('../models/Post')
 const Category = require('../models/Category')
+const User = require('../models/User')
 
 router.get('/', (req, res) => {
     console.log(req.session)
@@ -14,7 +15,7 @@ router.get('/', (req, res) => {
 
 router.get('/blog', (req, res) => {
 
-    Post.find({}).sort({$natural:-1}).then(posts => {
+    Post.find({}).populate({ path: 'author', model: User }).sort({$natural:-1}).then(posts => {
         Category.find({}).then(categories => {
             res.render('site2/blog', {posts:posts, categories: categories})
         })
