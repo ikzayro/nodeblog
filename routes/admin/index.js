9dd388861e8cc8ab8ab47a4ec71c0395c189f40d
Ceyhun <admnsts@hotmail.com>
2024-11-22 01:52:10 +0300

    admin post edit page added

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const Category = require('../../models/Category')
 const Post = require('../../models/Post')
 const router = express.Router()
+const path = require('path')
 
 
 /*.get */
@@ -39,6 +40,40 @@ router.get('/posts', (req, res) => {
     Post.find({}).populate({ path: 'category', model: Category }).sort({$natural:-1}).then(posts => {
             res.render('admin/posts', {posts:posts})
         
+    })
+})
+
+router.delete('/posts/:id', (req, res) => {
+     
+    Post.deleteOne({_id: req.params.id}).then(() => {
+        res.redirect('/admin/posts')
+    })
+})
+
+router.get('/posts/edit/:id', (req, res) => {
+     
+    Post.findOne({_id: req.params.id}).then(post => {
+        Category.find({}).then(categories => {
+            res.render('admin/editpost', {post: post, categories: categories})
+        })
+    })
+})
+
+router.put('/posts/:id', (req, res) => {
+    let post_image = req.files.post_image
+
+    post_image.mv(path.resolve(__dirname, '../../public/img/postimages', post_image.name))
+
+    Post.findOne({_id: req.params.id}).then(post => {
+        post.title = req.body.title
+        post.content = req.body.content
+        post.date = req.body.date
+        post.category = req.body.category
+        post.post_image = `/img/postimages/${post_image.name}`
+
+        post.save().then(post => {
+            res.redirect('/admin/posts')
+        })
     })
 })
 
